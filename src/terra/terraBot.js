b5b6e14d10e906e6a2535391cbba7660f1c8b63b
clement <hecquet.c59@gmail.com>
2022-02-03 15:16:11 +0100

    Added log system

--- src/terra/terraBot.js
+++ src/terra/terraBot.js
@@ -1,20 +1,22 @@
 import {randomEarthBot} from "./randomEarthBot.js";
 import {closeConnection, initConnection, renameCollection, retrieveCheapestItems} from "./terraDB.js";
 import {closeInfoDbConnection, initInfoDbConnection} from "./infoAndStatusDB/infoAndStatusDB.js";
+import {addToLogSystem} from "../logSystem.js";
 
 const main = async () => {
     await initConnection();
     await initInfoDbConnection();
     //const res = await retrieveCheapestItems('terra103z9cnqm8psy0nyxqtugg6m7xnwvlkqdzm4s4k', {}, 10)
     //res.forEach((e) => console.log(`${e.price} - ${e.name} - ${e.rank}`))
-
-    //randomEarthBot();
+    addToLogSystem("Launching the bot at " + Date.now());
+    randomEarthBot();
     //luartBot();
     //knowhereBot();
 }
 
 process.on('SIGINT', async () => {
     console.log('\nStopping the application properly');
+    await addToLogSystem("Stapping the bot at " + Date.now());
     await closeConnection();
     await closeInfoDbConnection();
     process.exit(0);
